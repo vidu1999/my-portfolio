ff4e43284e2eebf65f13e90cf3c14df1e2aee3f2
vidu1999 <vidurakavinda1999@gmail.com>
2026-04-30 11:17:39 +0530

    change name repoo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const repo = 'portfolio';
+const repo = 'my-portfolio';
 const basePath = isProd ? `/${repo}` : '';
 
 
